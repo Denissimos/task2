f437b34e6cdbd1664aeaa49999a03a5bf78ead73
Denissimos <Dragon.ru12@mail.ru>
2016-02-25 22:48:17 +0600

    Finding the roots of equation

--- main.c
+++ main.c
@@ -8,6 +8,15 @@ int main()
     scanf("%f %f %f", &a, &b, &c);
     D = (b * b) - (4 * a * c);
     D = sqrt(D);
+    if (D >= 0);
+    D = sqrt(D);
+    else {
+    printf("The discriminant is 0, no roots\n");
+    return 0;
+    }
+    x1 = (-b - D)/2 * a;
+    x2 = (-b + D)/2 * a;
+    printf("The roots of equation are %.2f %.2f \n, x1 x2");
 
 return 0;
 }

393271227769a894886c2e37e3e5481d3a8bce6e
Denissimos <Dragon.ru12@mail.ru>
2016-02-25 22:33:08 +0600

    Finding and removing the root of the discriminant

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 
 #include <stdio.h>
+#include <math.h>
 int main()
 {
+    float a, b, c, D, x1, x2;
+    printf("Input a, b, c\n");
+    scanf("%f %f %f", &a, &b, &c);
+    D = (b * b) - (4 * a * c);
+    D = sqrt(D);
 
 return 0;
 }
